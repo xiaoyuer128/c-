e67f2417e90d9eee1cbdfe9985d47b392d7ecb46
xiaoyuer <95510374+xiaoyuer128@users.noreply.github.com>
2022-05-07 15:55:11 +0800

    Update main.c

--- testwork0506/main.c
+++ testwork0506/main.c
@@ -159,3 +159,31 @@ int main()
 
     return 0;
 }
+/*
+#include <stdio.h>
+unsigned long  Fact(unsigned int  n);
+int main()
+{
+    int m, k;
+    double p;
+    do
+    {
+        printf("Input m,k (m>=k>0):");
+        scanf("%d,%d", &m, &k);
+    }
+    while (m < k || m < 0 || k < 0);
+    p = (double)Fact(m) / (Fact(k) * Fact(m - k));
+    printf("p = %.0f\n", p);
+    return 0;
+}
+unsigned long  Fact(unsigned int  n)
+{
+    unsigned int  i;
+    unsigned long result = 1;
+    for (i = 2; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+
+*/
